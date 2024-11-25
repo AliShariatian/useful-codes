e17a9f4e45fee3458ee9f549e06c88bc596acc45
alish-dev <ali.shariat.dev@gmail.com>
2024-11-25 16:51:59 +0330

    refactor(from-end): improve react-scan component

--- front-end/performance/react-scan/index.tsx
+++ front-end/performance/react-scan/index.tsx
@@ -1,22 +1,44 @@
 import { FC } from "react";
 import { isDevelopment } from "@/constant/global";
 
+interface IProps {
+  /**
+   * A flag that disables the injection of the `react-scan` script when set to `true`.
+   * @default false
+   */
+  disable?: boolean;
+}
+
 /**
- * ReactScan component for integrating the React Scan tool in development mode.
- *
- * This component is designed to be placed at the root layout of your application,
- * specifically at the top of the `<body>` tag. It conditionally includes the React Scan
- * script, enabling its usage only in development environments.
- *
- * Usage:
- * - Include this component in your `app/layout.js` or equivalent layout file.
- * - Ensure `isDevelopment` correctly reflects the environment (true for development).
+ * ReactScan is a React functional component that conditionally injects the `react-scan` script
+ * into the document `<head>` element. It is intended to be used only in development environments.
  *
  * @component
- * @returns {JSX.Element | null} A `<script>` element for React Scan if in development mode, otherwise `null`.
+ * @param {Object} props - The properties for the ReactScan component.
+ * @param {boolean} [props.disable=false] - When set to `true`, prevents the script from being injected.
+ * @returns {JSX.Element|null} A `<head>` element containing the script tag if conditions are met, otherwise `null`.
+ *
+ * @example
+ * // Example usage in a development-only context:
+ * import ReactScan from "./ReactScan";
+ *
+ * const App = () => {
+ *   return (
+ *     <>
+ *       <ReactScan disable={false} />
+ *
+ *       <body>Application Body</body>
+ *     </>
+ *   );
+ * };
+ *
+ * @remarks
+ * This component will only function correctly if the `isDevelopment` constant is `true`.
+ * Ensure that `isDevelopment` is configured properly to reflect your environment settings.
  */
-const ReactScan: FC = (): JSX.Element | null => {
-  if (!isDevelopment) return null;
+
+const ReactScan: FC<IProps> = ({ disable = false }): JSX.Element | null => {
+  if (!isDevelopment || disable) return null;
 
   return (
     // eslint-disable-next-line @next/next/no-head-element
