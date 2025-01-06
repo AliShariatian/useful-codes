4f532be6e73a212c9f700d8c3e3b9f569837e0fd
alish-dev <ali.shariat.dev@gmail.com>
2025-01-06 20:45:33 +0330

    add usage

--- common/typescript/global.ts
+++ common/typescript/global.ts
@@ -3,3 +3,14 @@ type MaximumOneOf<T, K extends keyof T = keyof T> = K extends keyof T
       [P in K]?: T[K];
     } & Partial<Record<Exclude<keyof T, K>, never>>
   : never;
+
+/**
+ * @usage
+ * type SetTTL = MaximumOneOf<{
+ * EX: number;
+ * PX: number;
+ * EXAT: number;
+ * PXAT: number;
+ * KEEPTTL: true;
+ *    }>;
+ */
